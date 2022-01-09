4f2ce55580206084dbcd79460cba7e7210677168
grzegab <greg@grzegab.eu>
2022-01-09 20:04:55 +0100

    feat(part-3): Simplify request param code

--- tasks/refactor.php
+++ tasks/refactor.php
@@ -1,17 +1,14 @@
 <?php
 
-if ($_REQUEST['email']) {
-    $masterEmail = $_REQUEST['email'];
-}
-$masterEmail = isset($masterEmail) && $masterEmail
-    ? $masterEmail
-    : array_key_exists('masterEmail', $_REQUEST) && $_REQUEST["masterEmail"]
-        ? $_REQUEST['masterEmail'] : 'unknown';
+$requestEmail = $_REQUEST['email'] ?? null;
+$requestMasterEmail = $_REQUEST["masterEmail"] ?? null;
 
+$masterEmail = $requestEmail ?? $requestMasterEmail ?? 'unknown';
 
 echo 'The master email is ' . $masterEmail . '\n';
+
 $conn = mysqli_connect('localhost', 'root', 'sldjfpoweifns', 'my_database');
-$res = mysqli_query($conn, "SELECT * FROM users WHERE email='" .
-                         $masterEmail . "'");
+$res = mysqli_query($conn, "SELECT * FROM users WHERE email='" . $masterEmail . "'");
 $row = mysqli_fetch_row($res);
+
 echo $row['username'] . "\n";
