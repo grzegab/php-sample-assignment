73dbbc3ba20c781844226914169f028fc064a30f
grzegab <greg@grzegab.eu>
2022-01-09 20:09:46 +0100

    feat(part-3): Check mysql connection to DB

--- tasks/refactor.php
+++ tasks/refactor.php
@@ -8,6 +8,12 @@ $masterEmail = $requestEmail ?? $requestMasterEmail ?? 'unknown';
 echo 'The master email is ' . $masterEmail . '\n';
 
 $conn = mysqli_connect('localhost', 'root', 'sldjfpoweifns', 'my_database');
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $res = mysqli_query($conn, "SELECT * FROM users WHERE email='" . $masterEmail . "'");
 $row = mysqli_fetch_row($res);
 
